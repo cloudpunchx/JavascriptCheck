2a673d3fbd03feecfc8571eb5d87ed72eee63432
cloudpunchx <siobhanbonardi@hotmail.com>
2022-12-01 20:40:08 -0700

    Started new project, wrote a few basic additions to the export default on script Example.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ScriptExample/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScriptExample from '@/components/ScriptExample.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScriptExample,
   }
 }
 </script>
---
+++ src/components/ScriptExample.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <p></p>
+        <p></p>
+        <p></p>
+    </div>
+</template>
+
+<!-- each key value is one of your variables -->
+<!-- usually life cycle hooks you keep it simple as possible and CALL FUNCTIONS, not write functions in them -->
+<script>
+    export default {
+        name : "ScriptExample",
+        data() {
+            return {
+                tweets : ["M-o-o-n, that spells I Love You", "Tweet 2", "Tweet 3"],
+                numFriends : 50,
+                isLoggedIn : true,
+            }
+        },
+        methods: {
+            showInfo() {
+                console.log("ShowInfo");
+                const myDiv = document.querySelector(`div`);
+                myDiv.insertAdjacentHTML('beforeend', `<h2>${this.numFriends}</h2>`);
+                // TERNARY OPERATOR OTHER WAY TO DO CONDITION BASED ONLY WORKS WITH TRUE OR FALSE 2 CONDITIONS
+                myDiv.insertAdjacentHTML(`beforeend`, `<h3>${this.isLoggedIn? "Welcome to the page!" : "User is not logged in."}`);
+                    // the Ternary Operator is equivalent to the if statement below:
+                // if(this.isLoggedIn){
+                //     myDiv.insertAdjacentHTML('beforeend', `<h3>Welcome to the page.</h3>`);
+                // }else{
+                //     myDiv.insertAdjacentHTML('beforeend', `<h3>User is not logged in.</h3>`);
+                // }
+                for (let tweet of this.tweets){
+                    myDiv.insertAdjacentHTML(`beforeend`, `<p>${tweet}</p>`)
+                }
+            }
+        },
+        mounted () {
+            this.showInfo();
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
